76f2ed5e0e92826b129390448ae23565b8550393
duc.dominh <duc.dominh@nashtechglobal.com>
2019-03-28 13:30:18 +0700

    update 28/3/2019

--- config/data_vocabulary.php
+++ config/data_vocabulary.php
@@ -77,72 +77,352 @@ return [
 	// 	'characters' => '時計(とけい)(tokei)',
 	// 	'mean' => 'đồng hồ'
 	// ],
-	19 => [
-		'characters' => '教室(きょうしつ)(Kyōshitsu)',
-		'mean' => 'phòng học'
+	// 19 => [
+	// 	'characters' => '教室(きょうしつ)(Kyōshitsu)',
+	// 	'mean' => 'phòng học'
+	// ],
+	// 20 => [
+	// 	'characters' => '食堂(しょくどう)(Shokudō)',
+	// 	'mean' => 'nhà ăn'
+	// ],
+	// 21 => [
+	// 	'characters' => '事務所(じむしょ)(Jimusho)',
+	// 	'mean' => 'văn phòng'
+	// ],
+	// 22 => [
+	// 	'characters' => '会議室(かいぎじつ)(Kaigijitsu)',
+	// 	'mean' => 'phòng họp'
+	// ],
+	// 23 => [
+	// 	'characters' => '(うち)(Uchi)',
+	// 	'mean' => 'nhà'
+	// ],
+	// 24 => [
+	// 	'characters' => '受付(うけつけ)(Uketsuke)',
+	// 	'mean' => 'quầy lễ tân'
+	// ],
+	// 25 => [
+	// 	'characters' => '部屋(へや)(Heya)',
+	// 	'mean' => 'căn phòng'
+	// ],
+	// 26 => [
+	// 	'characters' => '(トイレ)(Toire)',
+	// 	'mean' => 'nhà vệ sinh'
+	// ],
+	// 27 => [
+	// 	'characters' => '階段(かいだｎ)(Kaidan)',
+	// 	'mean' => 'cầu thang'
+	// ],
+	// 28 => [
+	// 	'characters' => '(エレベーター)(Erebētā)',
+	// 	'mean' => 'thang máy'
+	// ],
+	// 29 => [
+	// 	'characters' => '(エスカレーター)(Esukarētā)',
+	// 	'mean' => 'thang cuốn'
+	// ],
+	// 30 => [
+	// 	'characters' => '自動販売機(じどうはんばいき)(Jidō hanbaiki)',
+	// 	'mean' => 'máy bán hàng tự động'
+	// ],
+	// 31 => [
+	// 	'characters' => '国(くに)(kuni)',
+	// 	'mean' => 'nước(của bạn)'
+	// ],
+	// 32 => [
+	// 	'characters' => '会社(かいしゃ)(Kaisha)',
+	// 	'mean' => 'công ty'
+	// ],
+	// 33 => [
+	// 	'characters' => '靴(くつ)(Kutsu)',
+	// 	'mean' => 'giầy'
+	// ],
+	// 34 => [
+	// 	'characters' => '(ネクタイ)(Nekutai)',
+	// 	'mean' => 'cà vạt'
+	// ],
+	// 35 => [
+	// 	'characters' => '(ワイン)(Wain)',
+	// 	'mean' => 'rượu vang'
+	// ],
+	36 => [
+		'characters' => '食べる(たべる)(Taberu)',
+		'mean' => 'ăn'
 	],
-	20 => [
-		'characters' => '食堂(しょくどう)(Shokudō)',
-		'mean' => 'nhà ăn'
+	37 => [
+		'characters' => '飲み(のみ)(Nomi)',
+		'mean' => 'uống'
 	],
-	21 => [
-		'characters' => '事務所(じむしょ)(Jimusho)',
-		'mean' => 'văn phòng'
+	38 => [
+		'characters' => '(たばこをまい)(tabako wo mai)',
+		'mean' => 'hút thuốc lá'
 	],
-	22 => [
-		'characters' => '会議室(かいぎじつ)(Kaigijitsu)',
-		'mean' => 'phòng họp'
+	39 => [
+		'characters' => '見(み)(mi)',
+		'mean' => 'nhìn,xem'
 	],
-	23 => [
-		'characters' => '(うち)(Uchi)',
-		'mean' => 'nhà'
+	40 => [
+		'characters' => '聞き(きき)(kiki)',
+		'mean' => 'nghe'
 	],
-	24 => [
-		'characters' => '受付(うけつけ)(Uketsuke)',
-		'mean' => 'quầy lễ tân'
+	41 => [
+		'characters' => '読み(よみ)(yomi)',
+		'mean' => 'đọc'
 	],
-	25 => [
-		'characters' => '部屋(へや)(Heya)',
-		'mean' => 'căn phòng'
+	42 => [
+		'characters' => '書き(かき)(kaki)',
+		'mean' => 'viết,vẽ'
 	],
-	26 => [
-		'characters' => '(トイレ)(Toire)',
-		'mean' => 'nhà vệ sinh'
+	43 => [
+		'characters' => '買い(かい)(kai)',
+		'mean' => 'mua'
 	],
-	27 => [
-		'characters' => '階段(かいだｎ)(Kaidan)',
-		'mean' => 'cầu thang'
+	44 => [
+		'characters' => '写真を撮り(しゃしんをとり)(Shashin o tori)',
+		'mean' => 'chụp ảnh'
 	],
-	28 => [
-		'characters' => '(エレベーター)(Erebētā)',
-		'mean' => 'thang máy'
+	45 => [
+		'characters' => '(ともだちにあい)(Tomodachi ni ai)',
+		'mean' => 'gặp bạn bè'
 	],
-	29 => [
-		'characters' => '(エスカレーター)(Esukarētā)',
-		'mean' => 'thang cuốn'
+	46 => [
+		'characters' => '(ごはん)(Gohan)',
+		'mean' => 'bữa ăn, cơm'
 	],
-	30 => [
-		'characters' => '自動販売機(じどうはんばいき)(Jidō hanbaiki)',
-		'mean' => 'máy bán hàng tự động'
+	47 => [
+		'characters' => '朝ごはん(あさごはん)(Asa gohan)',
+		'mean' => 'bữa sáng'
 	],
-	31 => [
-		'characters' => '国(くに)(kuni)',
-		'mean' => 'nước(của bạn)'
+	48 => [
+		'characters' => '昼ごはん(ひるごはん)(Hiru gohan)',
+		'mean' => 'bữa trưa'
 	],
-	32 => [
-		'characters' => '会社(かいしゃ)(Kaisha)',
-		'mean' => 'công ty'
+	49 => [
+		'characters' => '(ばんごはん)(Ban gohan)',
+		'mean' => 'bữa tối'
 	],
-	33 => [
-		'characters' => '靴(くつ)(Kutsu)',
-		'mean' => 'giầy'
+	50 => [
+		'characters' => '(パン)(pan)',
+		'mean' => 'bánh mì'
 	],
-	34 => [
-		'characters' => '(ネクタイ)(Nekutai)',
-		'mean' => 'cà vạt'
+	51 => [
+		'characters' => '卵(たまご)(Tama go)',
+		'mean' => 'trứng'
 	],
-	35 => [
-		'characters' => '(ワイン)(Wain)',
-		'mean' => 'rượu vang'
+	52 => [
+		'characters' => '肉(にく)(niku)',
+		'mean' => 'thịt'
 	],
+	53 => [
+		'characters' => '魚(さかな)(Sakana)',
+		'mean' => 'cá'
+	],
+	54 => [
+		'characters' => '野菜(やさい)(Yasai)',
+		'mean' => 'rau'
+	],
+	55 => [
+		'characters' => '果物(くだもの)(Kudamono)',
+		'mean' => 'hoa quả, trái cây'
+	],
+	56 => [
+		'characters' => '水(みず)(Mizu)',
+		'mean' => 'nước'
+	],
+	57 => [
+		'characters' => 'お茶(おちゃ)(Ocha)',
+		'mean' => 'trà,trà xanh'
+	],
+	58 => [
+		'characters' => '紅茶(こうちゃ)(Kōcha)',
+		'mean' => 'trà hồng'
+	],
+	59 => [
+		'characters' => '牛乳(ぎゅうにゅう)(Gyūnyū)',
+		'mean' => 'sữa, sữa bò'
+	],
+	60 => [
+		'characters' => '(ジュース)(Jūsu)',
+		'mean' => 'nước hoa quả'
+	],
+	61 => [
+		'characters' => '(ビール)(Bīru)',
+		'mean' => 'bia'
+	],
+	62 => [
+		'characters' => 'お酒(おさけ)(o sake)',
+		'mean' => 'rượu,rượu gạo Nhật'
+	],
+	63 => [
+		'characters' => '(たばこ)(Tabako)',
+		'mean' => 'thuốc lá'
+	],
+	64 => [
+		'characters' => '手紙(てがみ)(Te ga mi)',
+		'mean' => 'thư (giấy)'
+	],
+	65 => [
+		'characters' => '報告する(ほこくする)(Hōkoku suru)',
+		'mean' => 'báo cáo'
+	],
+	66 => [
+		'characters' => '写真(しゃしん)(Shashin)',
+		'mean' => 'ảnh'
+	],
+	67 => [
+		'characters' => '(ビデオ)(Bideo)',
+		'mean' => 'băng video'
+	],
+	68 => [
+		'characters' => '店(みせ)(Mise)',
+		'mean' => 'cửa hàng'
+	],
+	69 => [
+		'characters' => '庭(にわ)(Ni wa)',
+		'mean' => 'vườn'
+	],
+	70 => [
+		'characters' => '宿題(しゅくだい)(Shukudai)',
+		'mean' => 'bài tập về nhà'
+	],
+	71 => [
+		'characters' => '(テニス)(Tenisu)',
+		'mean' => 'quần vợt'
+	],
+	72 => [
+		'characters' => '(サッカー)(Sakkā)',
+		'mean' => 'bóng đá'
+	],
+	// 73 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 74 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 75 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 76 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 77 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 78 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 79 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 80 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 81 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 82 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 83 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 84 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 85 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 86 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 87 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 88 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 89 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 90 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 91 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 92 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 93 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 94 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 95 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 96 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 97 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 98 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 99 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 100 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 101 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 102 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 103 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 104 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
+	// 105 => [
+	// 	'characters' => '()()',
+	// 	'mean' => ''
+	// ],
 ];
